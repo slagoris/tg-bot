24cfe406b70e3a1579bf6ef5255513c6ed541f90
Алексей <slag@inbox.ru>
2024-02-27 09:31:02 +0500

    Updates

--- assets/PageOne-B4MQjb1t.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as n,o}from"./index-BdbS5nyi.js";const t={name:"PageOne"};function a(c,r,s,_,p,f){return o(),n("h1",null,"Страница 1")}const m=e(t,[["render",a]]);export{m as default};
--- assets/PageTwo-C5NiF1xk.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as o,o as n}from"./index-BdbS5nyi.js";const t={name:"PageOne"};function a(c,r,s,_,p,f){return n(),o("h1",null,"Страница 2")}const m=e(t,[["render",a]]);export{m as default};
