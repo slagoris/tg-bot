fe14c314cecd62821b1832acd592fd144cfcd91d
Алексей <slag@inbox.ru>
2024-02-27 09:06:15 +0500

    Updates

--- assets/PageOne-BqjBdvPt.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as n,o}from"./index-9aRF3h5C.js";const t={name:"PageOne"};function a(c,r,s,_,p,f){return o(),n("h1",null,"Страница 1")}const m=e(t,[["render",a]]);export{m as default};
--- assets/PageTwo-BEIhp9Ks.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as o,o as n}from"./index-9aRF3h5C.js";const t={name:"PageOne"};function a(c,r,s,_,p,f){return n(),o("h1",null,"Страница 2")}const m=e(t,[["render",a]]);export{m as default};
