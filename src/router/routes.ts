c92cd4227685d1dba4002b88c26c2c9dfe6734da
Алексей <slag@inbox.ru>
2024-03-04 16:50:29 +0500

    Update routes.ts

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,17 +4,17 @@ export default [
         name: 'home',
         component: () => import('../pages/HomePage.vue'), 
     },
-    {
-        path: '/city',
-        name: 'city',
-        component: () => import('../pages/CitySelection.vue'), 
-    },
-    {
-        path: '/menu',
-        name: 'menu',
-        component: () => import('../pages/Menu.vue'), 
-    },
-    
+    // {
+    //     path: '/city',
+    //     name: 'city',
+    //     component: () => import('../pages/CitySelection.vue'), 
+    // },
+    // {
+    //     path: '/menu',
+    //     name: 'menu',
+    //     component: () => import('../pages/Menu.vue'), 
+    // },
+    //
 ]  // {
 //     path: '/',
 //     name: 'panel',
