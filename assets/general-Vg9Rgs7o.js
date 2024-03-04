8589054f68ef88f3dbbb4963b11005ef2d301500
Алексей <slag@inbox.ru>
2024-03-04 18:51:37 +0500

    Updates

--- assets/general-Vg9Rgs7o.js
+++
@@ -1,1 +0,0 @@
-import{k as a,r as n}from"./index-D7BWqSZ9.js";const i=a("generalStore",()=>{const t=n(),s=r=>{t.value=r},o=()=>t,e=n();return{currentLang:t,setLang:s,getLang:o,currentCity:e,getCity:()=>e,setCity:r=>{e.value=r}}});export{i as u};
