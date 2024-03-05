734c2a1b4cceac8103428c5d9303b5b35526efd2
Алексей <slag@inbox.ru>
2024-03-05 12:17:54 +0500

    Updates

---
+++ assets/general-SW6Wcqct.js
@@ -0,0 +1,1 @@
+import{l as g,r}from"./index-8LvpOBHH.js";const x=g("generalStore",()=>{const t=r(),s=e=>{t.value=e},o=()=>t,n=r(),c=e=>{n.value=e},u=()=>n,a=r();return{currentLang:t,setLang:s,getLang:o,currentCity:n,getCity:u,setCity:c,exchangePair:a,setExchangePair:e=>{a.value=e}}});export{x as u};
