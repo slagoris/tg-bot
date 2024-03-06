f2c3bfa6975c1b32baf17f771ddf2a54ac65cc5b
Алексей <slag@inbox.ru>
2024-03-06 16:45:36 +0500

    Update Menu.vue

--- src/pages/Menu.vue
+++ src/pages/Menu.vue
@@ -100,20 +100,20 @@ const backStep = () => {
   } 
 }
 const finishExchange = (data) => {
-    sendData(JSON.stringify(data))
+    // sendData(JSON.stringify(data))
     // console.log(data)
-    // fetch('https://test-bot-888.invi.pw/api/70013d3c-f63c-4e1d-8255-1895707e7980', {
-    //     method     : 'POST',
-    //     body       : JSON.stringify(data),
-    //     credentials: 'include',
-    //     headers    : {'Content-Type': 'application/json'}
-    // }).then(function (response) {
-    //     return response.json();
-    // }).then(function (result) {
-    //     // onCallback && onCallback(result);
-    // }).catch(function (error) {
-    //     console.log(error)
-    // });
+    fetch('https://test-bot-888.invi.pw/api/70013d3c-f63c-4e1d-8255-1895707e7980', {
+        method     : 'POST',
+        body       : JSON.stringify(data),
+        credentials: 'include',
+        headers    : {'Content-Type': 'application/json'}
+    }).then(function (response) {
+        return response.json();
+    }).then(function (result) {
+        // onCallback && onCallback(result);
+    }).catch(function (error) {
+        console.log(error)
+    });
     exchangeDialog.value = false
     notificationOccurred('success')
     showAlert('Успешно, ваша заявка принята. Отправили вам детали обмена.')
